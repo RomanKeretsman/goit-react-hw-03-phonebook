676e533e11b05d73f8dec4da0f26f5fdba2d708a
Roman Keretsman <rkeretsman@gmail.com>
2021-05-18 19:02:19 +0300

    added saving contacts to local storage

--- src/App.js
+++ src/App.js
@@ -10,14 +10,25 @@ import styles from './App.module.scss';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parcedContacts = JSON.parse(contacts);
+    if (parcedContacts) {
+      this.setState({
+        contacts: parcedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addNewContact = (name, number) => {
     const contact = {
